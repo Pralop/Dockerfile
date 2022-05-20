e25c31915bb4d910a0f595d5c08826ba4445c384
Pralop <105304041+Pralop@users.noreply.github.com>
2022-05-20 16:25:24 +0700

    Update shell script

--- setup_docker.sh
+++ setup_docker.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 sudo yum update -y
+echo "Latest security patch of Amazon Linux"
 sudo amazon-linux-extras install docker -y
+sudo service docker start
+echo "Install Docker and Start service completed"
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo chmod 666 /var/run/docker.sock
 sudo chown ec2-user /var/run/docker.sock
+echo "Install Docker-Compose completed"
 sudo yum install git -y
-sudo service docker start
+echo "Install git completed"
 echo "Please run docker info and docker-compose --version for check version and service after install"
